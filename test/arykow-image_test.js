8a82917c83c70fe90d18e83e15595f2d8788b1ab
Karim DRIDI <karim.dridi@gmail.com>
2013-12-06 09:25:37 +0100

    fromBuffer, fromFile and fromURI functions

--- test/arykow-image_test.js
+++ test/arykow-image_test.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var arykow_image = require('../lib/arykow-image.js');
+var arykow = {
+  image: require('../lib/arykow-image.js')
+};
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -22,15 +24,251 @@ var arykow_image = require('../lib/arykow-image.js');
     test.ifError(value)
 */
 
-exports['awesome'] = {
+exports['fromBuffer'] = {
   setUp: function(done) {
     // setup here
     done();
   },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(arykow_image.awesome(), 'awesome', 'should be awesome.');
-    test.done();
+  'failureFromBuffer01': function(test) {
+    arykow.image.fromBuffer().then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+  'failureFromBuffer02': function(test) {
+    arykow.image.fromBuffer(null).then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+  'failureFromBuffer03': function(test) {
+    arykow.image.fromBuffer(1345).then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+  'failureFromBuffer04': function(test) {
+    arykow.image.fromBuffer(new Buffer('')).then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+};
+
+exports['fromFile'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'failureFromFile01': function(test) {
+    arykow.image.fromFile().then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+  'failureFromFile02': function(test) {
+    arykow.image.fromFile(null).then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+  'failureFromFile03': function(test) {
+    arykow.image.fromFile(1345).then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+  'failureFromFile04': function(test) {
+    arykow.image.fromFile('test/data/01.txt').then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+  'failureFromFile05': function(test) {
+    arykow.image.fromFile('test/data/01.not.exists').then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+  'successFromFile06': function(test) {
+    arykow.image.fromFile('test/data/01.gif').then(function(result) {
+      test.expect(5);
+      test.notEqual(result, null);
+      test.notEqual(result.data, null);
+      test.notEqual(result.name, null);
+      test.notEqual(result.extension, null);
+      test.equal(result.toHTML(), 'data:image/gif;base64,R0lGODdhAQABAIAAAMzMzJaWliwAAAAAAQABAAACAkQBADs=');
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.equal(error, null);
+      test.done();
+    });
+  },
+  'successFromFile07': function(test) {
+    arykow.image.fromFile('test/data/01.png').then(function(result) {
+      test.expect(5);
+      test.notEqual(result, null);
+      test.notEqual(result.data, null);
+      test.notEqual(result.name, null);
+      test.notEqual(result.extension, null);
+      test.equal(result.toHTML(), 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAABlBMVEXMzMyWlpYU2uzLAAAACklEQVQImWNgAAAAAgAB9HFkpgAAAABJRU5ErkJggg==');
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.equal(error, null);
+      test.done();
+    });
+  },
+};
+
+exports['fromURI'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'failureFromURI01': function(test) {
+    arykow.image.fromURI().then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+  'failureFromURI02': function(test) {
+    arykow.image.fromURI(null).then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+  'failureFromURI03': function(test) {
+    arykow.image.fromURI(1345).then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+  'failureFromURI04': function(test) {
+    arykow.image.fromURI('http://www.google.com/placehold.it/1x1.gif').then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+  'failureFromURI05': function(test) {
+    arykow.image.fromURI('htt:p://placehold.it/1x1.gif').then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.done();
+    });
+  },
+  'failureFromURI06': function(test) {
+    arykow.image.fromURI('http://placehold.it/1x1.gif').then(function(result) {
+      test.expect(5);
+      test.notEqual(result, null);
+      test.notEqual(result.data, null);
+      test.notEqual(result.name, null);
+      test.notEqual(result.extension, null);
+      test.equal(result.toHTML(), 'data:image/gif;base64,R0lGODdhAQABAIAAAMzMzJaWliwAAAAAAQABAAACAkQBADs=');
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.equal(error, null);
+      test.done();
+    });
+  },
+  'failureFromURI07': function(test) {
+    arykow.image.fromURI('http://placehold.it/1x1.png').then(function(result) {
+      test.expect(5);
+      test.notEqual(result, null);
+      test.notEqual(result.data, null);
+      test.notEqual(result.name, null);
+      test.notEqual(result.extension, null);
+      test.equal(result.toHTML(), 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAABlBMVEXMzMyWlpYU2uzLAAAACklEQVQImWNgAAAAAgAB9HFkpgAAAABJRU5ErkJggg==');
+      test.done();
+    }, function(error) {
+      test.expect(2);
+      test.equal(error, null);
+      test.done();
+    });
   },
 };
